cb201ec3ee1ec458939136d82e6536faa63ce042
Nour <>
2020-12-13 20:38:41 -0500

    tests work

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -13,21 +13,26 @@ public class NoNullArrayList<T> extends ArrayList<T>{
   public void add(int index, T element){
     if (element==null){
       throw new IllegalArgumentException("Element cannot be null");
+    }else{
+      super.add(index, element);
     }
-    super.add(index, element);
   }
 
   public boolean add (T e){
     if (e==null){
       throw new IllegalArgumentException("Element cannot be null");
+    }else{
+      super.add(e);
+      return true;
     }
-    super.add(e);
-    return true;
   }
 
   public T set(int index, T element){
-    super.set(index, element);
-    return element;
+    if (element==null){
+      throw new IllegalArgumentException("Element cannot be null");
+    }else{
+      return super.set(index, element);
+    }
   }
 
   public static void main(String[] args){
